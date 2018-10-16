5d1b50115c3145c2caa71e5a7881b4e83bae0be6
Patrick El-Hage <patrick.elhage@gmail.com>
2018-10-16 01:43:53 -0700

    Remove console

--- src/index.js
+++ src/index.js
@@ -6,6 +6,5 @@ const QUERY_PARAMS = require('./api').QUERY_PARAMS
 const outputWords = require('./console/pretty-print').outputWords
   
 const query = querystring.stringify(mapOptionsToQuery(CLI, QUERY_PARAMS))
-console.log('suu')
 
 getWords(query).then(data => outputWords(data, { isVerbose: true, isSorted: true }))
