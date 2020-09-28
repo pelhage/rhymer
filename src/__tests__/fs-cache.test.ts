993f83441892d6a95567bee64fe5b43a1e698463
Patrick El-Hage <patrick.elhage@gmail.com>
2020-09-27 23:39:48 -0700

    add getters and setter validation

--- src/__tests__/fs-cache.test.ts
+++ src/__tests__/fs-cache.test.ts
@@ -5,6 +5,60 @@ describe('local cache', () => {
     it('instantiates an object to access the cache', () => {
       const cacheInstance = makeCache()
       expect(cacheInstance).toHaveProperty('getRhyme')
+      expect(cacheInstance).toHaveProperty('get')
+      expect(cacheInstance).toHaveProperty('set')
+    })
+    it('can instantiate the cache with pre-existing data', () => {
+      const cacheInstance = makeCache({ foo: 'bar' })
+      const actual = cacheInstance.get('foo')
+      const expected = 'bar'
+
+      expect(actual).toBe(expected)
+    })
+  })
+  describe('setting values', () => {
+    it('sets values for the cache', () => {
+      const cacheInstance = makeCache()
+      cacheInstance.set('foo', 'bar')
+      const actual = cacheInstance.get('foo')
+      const expected = 'bar'
+
+      expect(actual).toBe(expected)
+    })
+    it('should throw if the key is not a string', () => {
+      const cacheInstance = makeCache()
+      expect(() => cacheInstance.set(55, 'bar')).toThrow()
+      expect(() => cacheInstance.set(undefined, 'bar')).toThrow()
+      expect(() => cacheInstance.set(null, 'bar')).toThrow()
+      expect(() => cacheInstance.set(NaN, 'bar')).toThrow()
+      expect(() => cacheInstance.set({}, 'bar')).toThrow()
+      expect(() => cacheInstance.set([], 'bar')).toThrow()
+      expect(() => cacheInstance.set(() => {}, 'bar')).toThrow()
+    })
+  })
+  describe('getting values', () => {
+    it('reads values from the cache', () => {
+      const cacheInstance = makeCache({ foo: 'baz' })
+      const actual = cacheInstance.get('foo')
+      const expected = 'baz'
+
+      expect(actual).toBe(expected)
+    })
+    it('returns undefined for values not in the cache', () => {
+      const cacheInstance = makeCache()
+      const actual = cacheInstance.get('foo')
+      const expected = undefined
+      expect(actual).toBe(expected)
+    })
+    it('should throw if the key is not a string', () => {
+      const cacheInstance = makeCache()
+      expect(() => cacheInstance.get(55)).toThrow()
+      expect(() => cacheInstance.get(undefined)).toThrow()
+      expect(() => cacheInstance.get(null)).toThrow()
+      expect(() => cacheInstance.get(NaN)).toThrow()
+      expect(() => cacheInstance.get({})).toThrow()
+      expect(() => cacheInstance.get([])).toThrow()
+      expect(() => cacheInstance.get(() => {})).toThrow()
     })
   })
 })
--- src/core/cache.ts
+++ src/core/cache.ts
@@ -1,14 +1,21 @@
-export const makeCache = () => {
-  const cache = {
+export const makeCache = (
+  cache: Record<string, string> = {
     cash: 'cache'
   }
+) => {
   return {
     get(key) {
+      if (typeof key !== 'string') {
+        throw Error()
+      }
       if (cache[key]) {
         return cache[key]
       }
     },
     set(key, value) {
+      if (typeof key !== 'string') {
+        throw Error()
+      }
       cache[key] = value
     },
     getRhyme() {
